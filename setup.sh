fbcd3b72665456e85217ce99c647d8146f0bce5c
scscp <143437421+scscp@users.noreply.github.com>
2025-10-05 12:50:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
-#!/usr/bin/env bash
-pkg update -y
-pkg install python -y
-pkg install python-pillow -y
+##!/usr/bin/env bash
+python3.12 -m venv .venv
+source .venv/bin/activate
+
+apt update -y
+apt install python-pillow -y
 pip install -r requirements.txt
 pip install --upgrade rich
+
+
+# termux
+# pkg update -y
+# pkg install python -y
+# pkg install python-pillow -y
+# pip install -r requirements.txt
+# pip install --upgrade rich
